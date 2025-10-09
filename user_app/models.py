61679ffa4520b49e6e71a8a49cf809b814006006
Mitra_Ghahramani <mitrayamgh1996@gmIL.com>
2025-10-09 16:25:21 +0330

    create userprofile model,register it,Media and static file settings are done.

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class userprofile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,blank=True,null=True)
+    name=models.CharField(max_length=100,blank=True,null=True)
+    username=models.CharField(max_length=100,blank=True,null=True)
+    bio=models.TextField(max_length=1000,blank=True,null=True)
+    email=models.EmailField(blank=True,null=True)
+    phone_number=models.CharField(max_length=11,null=True,blank=True)
+    address=models.CharField(max_length=500,blank=True,null=True)
+    created=models.DateField(auto_now_add=True)
+    image=models.ImageField(blank=True,null=True,upload_to="user_app/images/",default="user_app/images/default.jpg")
