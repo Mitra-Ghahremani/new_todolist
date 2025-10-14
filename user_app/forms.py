e58793f06a04eab4fad2f3047b8b0369eb9db290
Mitra_Ghahramani <mitrayamgh1996@gmIL.com>
2025-10-14 21:28:18 +0330

    added userProfilForm

--- user_app/forms.py
+++ user_app/forms.py
@@ -7,3 +7,8 @@ class UserForm(forms.ModelForm):
     class Meta:
         model=User
         fields=["username","first_name","last_name","email","password","password"] 
+
+class userProfileForm(forms.ModelForm):
+    class Meta:
+        model=userProfile
+        exclude=['user']
