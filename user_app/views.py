1465fcac0e4009ff5f8e8760302c84334ee6d80f
Mitra_Ghahramani <mitrayamgh1996@gmIL.com>
2025-10-15 22:22:53 +0330

    modified views/editProfile and views/profileUser

--- user_app/views.py
+++ user_app/views.py
@@ -23,13 +23,14 @@ def registerUser(request):
             #برقرار رابطه ی بین پروفایل و یوزر
             profile.user=user
             profile.save()
-            return redirect('user_app:home')
-        #اگر فرمها معتبر نبودند،ارور های فرم ها نمایش داده شود
+            return redirect('user_app:my_account')
+       
         return render(request,'user_app/register.html',context={'user':user,'profile':profile})
-    
     user=UserForm()
     profile=UserProfileForm()
     return render(request,"user_app/register.html",context={'user':user,'profile':profile})
+
+
 
 def loginUser(request):
     form=LoginForm()
@@ -39,25 +40,20 @@ def loginUser(request):
         user=authenticate(username=username,password=password)
         if user:
             login(request,user)
-        #اگر کاربر ناشناس بود یعنی ثبت نام نکرده بود؟؟؟    
+         
         return render(request,'user_app/login.html',context={'form':form})
 
     return render(request,'user_app/login.html',context={'form':form})
 
 
-
+@login_required
 def profileUser(request):
-    #کاربر باید احراز هویت شده باشد تا پروفایلش را ببیند!
-    if request.user.is_authenticated:
-        profile=userProfile.objects.get(user=request.user)
+        profile=get_object_or_404(userProfile,user=request.user)
         return render(request,'user_app/profile.html',context={'profile':profile})
-    #اگر احراز هویت نشده بود به صفحه لاگین منتقل شود
-    return redirect ('user_app:login')
 
 @login_required   
 def editProfile(request,pk):
-        #خواندن آبجکت مرتبط با آیدی درخواستی از دیتابیس
-        profileObject=userProfile.objects.get(id=pk)
+        profileObject=get_object_or_404(userProfile,id=pk)
         profile=UserProfileForm(instance=profileObject)
         user=userEditForm(instance=request.user) 
         if request.method=='POST':
