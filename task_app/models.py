d677a2c916a57441751253010a409d3db02a7d81
unknown <mitrayamgh1996@gmail.com>
2025-12-15 11:01:58 +0330

    the order of writting models has changed

--- task_app/models.py
+++ task_app/models.py
@@ -3,18 +3,22 @@ from django.contrib.auth.models import User
 
 
 
-class Task(models.Model):
-    description=models.TextField(max_length=150)
-    created_date=models.DateTimeField(auto_now=True)
-    status=models.BooleanField(default=False)
-    def __str__(self):
-        return self.description
-    
-    
+
 class TODOLIST(models.Model):
-    task=models.ForeignKey(Task,on_delete=models.CASCADE,null=True,blank=True)
+  
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.user.username
     
+
+class Task(models.Model):
+    description=models.TextField(max_length=150)
+    created_date=models.DateTimeField(auto_now=True)
+    status=models.BooleanField(default=False)
+    todolist=models.ForeignKey(TODOLIST,on_delete=models.CASCADE,blank=True)
+    def __str__(self):
+        return self.description
+    
+    
+
 
