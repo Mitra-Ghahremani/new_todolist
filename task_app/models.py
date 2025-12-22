46d3b33b1177fb1f8d6b60436cb6591a4d92290c
unknown <mitrayamgh1996@gmail.com>
2025-12-22 11:24:37 +0330

    fixed conflict

--- task_app/models.py
+++ task_app/models.py
@@ -5,35 +5,23 @@ from django.contrib.auth.models import User
 
 
 
-class TODOLIST(models.Model):
-  
+class ToDoList(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.user.username
     
 
 class Task(models.Model):
-<<<<<<< HEAD
     description = models.TextField(max_length=150)
     created_date = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=False)
-=======
     description=models.TextField(max_length=150)
     created_date=models.DateTimeField(auto_now=True)
     status=models.BooleanField(default=False)
-    todolist=models.ForeignKey(TODOLIST,on_delete=models.CASCADE,blank=True)
->>>>>>> 4fb3f105ac882c8eb8debcaf5c7d7596e6057772
+    todolist=models.ForeignKey(ToDoList,on_delete=models.CASCADE,blank=True)
     def __str__(self):
         return self.todolist.user.username
     
     
-<<<<<<< HEAD
-class ToDoList(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.user.username
-=======
->>>>>>> 4fb3f105ac882c8eb8debcaf5c7d7596e6057772
 
 
--- task_app/views.py
+++ task_app/views.py
@@ -9,7 +9,6 @@ from .forms import CreateTaskForm
 
 
 @login_required
-<<<<<<< HEAD
 def create_task(request):
     empty_form = CreateTaskForm()       
     if request.method == 'POST':
@@ -26,11 +25,12 @@ def create_task(request):
     return render(request, 
                   "task_app/create_task.html", 
                   context={"form":empty_form})
-=======
+
+
 def createTask(request):
-    empty_form=createTaskForm()
+    empty_form=CreateTaskForm()
     if request.method=='POST':
-      form=createTaskForm(request.POST)
+      form=CreateTaskForm(request.POST)
       if form.is_valid():
          form=form.save(commit=False)
          form.todolist_id=request.user.id
@@ -39,12 +39,11 @@ def createTask(request):
          return render(request,"task_app/create_task.html",
                        context={"form":empty_form,"message":"تسک شما با موفقیت اضافه شد"})
     return render(request,"task_app/create_task.html",context={"form":empty_form})
->>>>>>> 4fb3f105ac882c8eb8debcaf5c7d7596e6057772
+
 
 
 
 @login_required
-<<<<<<< HEAD
 def display_task(request):
     todolist = ToDoList.objects.filter(user=request.user)
     if todolist:
@@ -55,14 +54,14 @@ def display_task(request):
 
     return HttpResponse ("تو دو لیست برای این کاربر وجود ندارد")
 
-=======
+
 def displayTask(request):
    task=Task.objects.filter(todolist_id=request.user.id)
    print(task)
    if task:
        return render(request,"task_app/display_task.html",context={"task":task})
    return  HttpResponse ("برای این کاربر تسکی وجود ندارد")
->>>>>>> 4fb3f105ac882c8eb8debcaf5c7d7596e6057772
+
 
 @login_required   
 def edit_task(request, id):
